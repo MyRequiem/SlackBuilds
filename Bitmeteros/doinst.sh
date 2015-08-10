075342083aa162c43253a43d00a97cf7c2be9ce3
MyRequiem <mailto:mrvladislavovich@gmail.com>
2015-08-10 14:27:15 +0300

    first commit

---
+++ Bitmeteros/doinst.sh
@@ -0,0 +1,38 @@
+# create or upgrade database
+DB_FILE=/var/lib/bitmeter/bitmeter.db
+
+if [ -f ${DB_FILE} ]; then
+	# if database already exist
+	rm -f ${DB_FILE}.new
+	bmdb upgrade 7
+else
+	mv ${DB_FILE}.new ${DB_FILE}
+fi
+
+BTM=/etc/init.d/bitmeter
+BTM_WEB=/etc/init.d/bitmeterweb
+
+# start
+${BTM} start
+${BTM_WEB} start
+
+# autostart
+RC_LOCAL=/etc/rc.d/rc.local
+
+if ! grep -q "${BTM} start" ${RC_LOCAL}; then
+cat << EOF >> ${RC_LOCAL}
+# Starting BitMeter Capture daemon: bmcapture 
+if [ -x ${BTM} ]; then
+	${BTM} start
+fi
+
+# Starting BitMeter Web Interface daemon: bmws
+if [ -x ${BTM_WEB} ]; then
+	${BTM_WEB}  start
+fi
+
+EOF
+fi
+
+echo -e "Type in the browser: http://localhost:2605/index.html\n"
+
