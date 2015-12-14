5a32990622a9ebc30b7bf624e37c48a65a88af5e
MyRequiem <mailto:mrvladislavovich@gmail.com>
2015-12-14 15:55:23 +0300

    modified Bitmeteros && createpackfromsrc

--- Bitmeteros/doinst.sh
+++ Bitmeteros/doinst.sh
@@ -2,11 +2,11 @@
 DB_FILE=/var/lib/bitmeter/bitmeter.db
 
 if [ -f ${DB_FILE} ]; then
-	# if database already exist
-	rm -f ${DB_FILE}.new
-	bmdb upgrade 7
+    # if database already exist
+    rm -f ${DB_FILE}.new
+    bmdb upgrade 7
 else
-	mv ${DB_FILE}.new ${DB_FILE}
+    mv ${DB_FILE}.new ${DB_FILE}
 fi
 
 BTM=/etc/init.d/bitmeter
@@ -21,14 +21,14 @@ RC_LOCAL=/etc/rc.d/rc.local
 
 if ! grep -q "${BTM} start" ${RC_LOCAL}; then
 cat << EOF >> ${RC_LOCAL}
-# Starting BitMeter Capture daemon: bmcapture 
+# Starting BitMeter Capture daemon: bmcapture
 if [ -x ${BTM} ]; then
-	${BTM} start
+    ${BTM} start
 fi
 
 # Starting BitMeter Web Interface daemon: bmws
 if [ -x ${BTM_WEB} ]; then
-	${BTM_WEB}  start
+    ${BTM_WEB}  start
 fi
 
 EOF
