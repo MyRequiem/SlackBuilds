dda36fbee616e706d6d291c96709f97a4cbcb688
MyRequiem <mailto:mrvladislavovich@gmail.com>
2016-11-24 12:51:07 +0400

    modified createpackfromsrc, deleted: Opera-next, finch, phpStorm, vim

--- phpStorm/doinst.sh
+++
@@ -1,5 +0,0 @@
-if [ -f /opt/phpstorm/jre/jre/lib/amd64/libgstreamer-lite.so ]; then
-    ( cd usr/lib64 ; rm -rf libgstreamer-lite.so )
-    ( cd usr/lib64 ; ln -sf /opt/phpstorm/jre/jre/lib/amd64/libgstreamer-lite.so libgstreamer-lite.so)
-fi
-
