3d1f40f7ce0427101e23c1638ad431a8ed124696
Vladimir <mrvladislavovich@gmail.com>
2019-09-25 19:01:16 +0400

    npm2txz/npm2txz.sh updated

--- npm2txz/npm2txz.sh
+++ npm2txz/npm2txz.sh
@@ -1,39 +1,45 @@
 #!/bin/sh
 
-PKGNAME="$1"
-
 SCRIPT_NAME="$(basename "$0")"
 if [[ "x$(id -u)" == "x0" ]]; then
     echo "${SCRIPT_NAME} should be run as a normal user."
     exit 1
 fi
 
+# check npm command
+if ! command -v npm &>/dev/null; then
+    echo "npm: command not found."
+    echo -n "You need to install package \"nodejs\" or "
+    echo "\"nodejs-bin\" from SBo repository."
+    exit 1
+fi
+
 # check param (node module name)
-if [[ "x${PKGNAME}" == "x" || \
-        "x${PKGNAME}" == "x--help" || \
-        "x${PKGNAME}" == "x-h" ]]; then
+if [[ "x$1" == "x" || \
+        "x$1" == "x--help" || \
+        "x$1" == "x-h" ]]; then
     echo "Usage: ${SCRIPT_NAME} module_name"
     exit 0
 fi
 
-# check npm command
-if ! which npm 1>/dev/null 2>&1; then
-    echo "npm: command not found."
-    echo "You need to install package \"nodejs\" from SBo repository."
-    exit 1
-fi
-
+# package name can be specified with a postfix or without it:
+#    development or pre-release version
+#       $ ./nmp2txz.sh instant-markdown-d@next
+#    latest stable version
+#       $ ./nmp2txz.sh instant-markdown-d
+#       $ ./nmp2txz.sh instant-markdown-d@latest
+PKGNAME=$(echo "$1" | cut -d "@" -f 1)
 BUILD=""
 TAG="myreq"
 PKGTYPE="txz"
 OUTPUT="$(pwd)"
 TMP="/tmp/${PKGNAME}-build/"
 
-rm -rf "${TMP}"
+sudo rm -rf "${TMP}"
 mkdir -p "${TMP}"
 cd "${TMP}" || exit 1
 
-DESTDIR=./ npm install -g "${PKGNAME}" || exit 1
+DESTDIR=./ npm install -g "$1" || exit 1
 
 ARCH=""
 [ "$(uname -m)" == "x86_64" ] && ARCH="64"
@@ -56,7 +62,7 @@ elif [ -n "${ARCH}" ]; then
         for FILE in ${FILES}; do
             if [ -L "${FILE}" ]; then
                 TARGET=$(readlink "${FILE}")
-                rm "${FILE}"
+                sudo rm "${FILE}"
                 ln -s "$(echo "${TARGET}" | sed -e 's/\/lib\//\/lib64\//g')" \
                     "${FILE}"
             fi
@@ -67,10 +73,10 @@ fi
 # remove empty /usr/etc directory
 [ -d usr/etc ] && \
     [[ "x$(find usr/etc -type f -o -type l  | wc -l)" == "x0" ]] && \
-    rm -rf usr/etc
+    sudo rm -rf usr/etc
 
 JSON="usr/lib${ARCH}/node_modules/${PKGNAME}/package.json"
-VERSION=$(grep -i "\"version\":" "${JSON}" | cut -d \" -f 4 | tr - .)
+VERSION=$(grep -i "\"version\":" "${JSON}" | cut -d \" -f 4 | tr "-" "_")
 DESCRIPTION=$(grep -i "\"description\":" "${JSON}" | cut -d \" -f 4)
 HOMEPAGE=$(grep -i "\"homepage\":" "${JSON}" | cut -d \" -f 4)
 
